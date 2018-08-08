c96267a5cd04f6d46180299e5f406913aa0a9827
Elmo Neto <eneto94@gmail.com>
2018-08-08 00:28:54 -0300

    remoção de código da classe de teste de vetores

--- aula-31-07-18/trab1/TestaVetores.java
+++ aula-31-07-18/trab1/TestaVetores.java
@@ -4,7 +4,5 @@ public class TestaVetores
 {
    public static void main(String[] args)
    {
-      Vet2 vetorTeste = new Vet2(8,6);
-      System.out.print("Tamanho do vetor: " + vetorTeste.calculaTamanho());
    }
 }
